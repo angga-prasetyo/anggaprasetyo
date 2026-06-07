7fa6b7d9ce4372b22b772735c88f148e8b813a8d
Angga <69108401+angga-prasetyo@users.noreply.github.com>
2026-06-07 12:02:11 +0700

    add additional words

--- src/constants/languages.ts
+++ src/constants/languages.ts
@@ -12,6 +12,14 @@ export const words: {
     [LANGUAGES.IDN]: 'Izinkan',
     [LANGUAGES.ENG]: 'Allow',
   },
+  indonesia: {
+    [LANGUAGES.IDN]: 'Indonesia',
+    [LANGUAGES.ENG]: 'Inggris (US)',
+  },
+  english: {
+    [LANGUAGES.IDN]: 'Indonesia',
+    [LANGUAGES.ENG]: 'English (US)',
+  },
   popup__enable_sound_title: {
     [LANGUAGES.IDN]: 'IZINKAN PUTAR SUARA?',
     [LANGUAGES.ENG]: 'ALLOW PLAY SOUND?',
@@ -19,5 +27,9 @@ export const words: {
   popup__enable_sound_description: {
     [LANGUAGES.IDN]: 'Izinkan untuk mendapatkan pengalaman terbaik',
     [LANGUAGES.ENG]: 'Allow to get the best experience.',
+  },
+  popup__choose_language_title: {
+    [LANGUAGES.IDN]: 'Pilih Bahasa',
+    [LANGUAGES.ENG]: 'Choose Language',
   },
 };
