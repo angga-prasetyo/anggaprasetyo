d4fd4c4f5954151fdc1f4b662b9f0cc866def0c7
Angga <69108401+angga-prasetyo@users.noreply.github.com>
2026-06-15 12:36:22 +0700

    add name role section

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -23,7 +23,30 @@ const HomePage: React.FC = () => {
       />
 
       {/* Home Content */}
-      <div className="flex items-center gap-4">adasdas</div>
+      {/* <div className="bg-[#080C10]">
+        <h2 className="text-[#00c9d4] text-2xl font-bold pt-30">
+          Enim elit veniam sit ullamco magna.
+        </h2>
+        <h2 className="text-[#C0392B] text-2xl font-bold">
+          Enim elit veniam sit ullamco magna.
+        </h2>
+        <h2 className="text-[#E8F4F4] text-2xl font-bold">
+          Enim elit veniam sit ullamco magna.
+        </h2>
+         <h2 className="text-[#E8E8DC] text-2xl font-bold">
+          Enim elit veniam sit ullamco magna.
+        </h2>
+      </div> */}
+      <div className="pt-15 flex justify-end">
+        <div className="relative bg-[#596266] w-60 mr-5">
+          <h2 className="absolute text-[#E8E8DC] text-xl font-bold -top-4 left-3">
+            Angga Prasetyo
+          </h2>
+          <h2 className="text-[#E8E8DC] text-md font-bold pt-4 pb-2 pl-3">
+            Remote Frontend Engineer
+          </h2>
+        </div>
+      </div>
     </CTLayout>
   );
 };
