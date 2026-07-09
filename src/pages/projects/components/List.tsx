0de6e327c50b8ddf18044b857ffbe51ba4e591f7
Angga <69108401+angga-prasetyo@users.noreply.github.com>
2026-07-09 13:26:54 +0700

    add list container

--- src/pages/projects/components/List.tsx
+++ src/pages/projects/components/List.tsx
@@ -64,18 +64,33 @@ export const List: React.FC = () => {
     <div className="w-full h-[81%] overflow-scroll">
       {list[topic].map(({ id, companyName, projectName }) => {
         return (
-          <div
+          <details
             key={id}
-            className="flex justify-between items-center w-full py-1.5 px-2 mb-2 bg-[#403f3f]">
-            {/* Company Name */}
-            <div className="flex gap-2">
-              <ChevronRight className="text-amber-300 animate-pulse" />
-              <h5 className="text-sm text-[#a5a4a4]">{companyName}</h5>
-            </div>
+            className="group [&_summary::-webkit-details-marker]:hidden details-content:max-h-0 details-content:opacity-0 details-content:-translate-y-5 details-content:overflow-hidden details-content:transition-[max-height,opacity,content-visibility,translate] details-content:duration-500 details-content:ease-in-out details-content:transition-discrete open:details-content:max-h-[50%] open:details-content:opacity-100 open:details-content:translate-y-0">
+            <summary className="flex justify-between items-center w-full py-1.5 px-2 mb-2 transition-[margin-bottom] duration-500 bg-[#403f3f] group-open:mb-0">
+              {/* Company Name */}
+              <div className="flex gap-2">
+                <ChevronRight className="text-amber-300 animate-pulse transition-transform duration-300 group-open:rotate-90" />
+                <h5 className="text-sm text-[#a5a4a4]">{companyName}</h5>
+              </div>
 
-            {/* Project Name  */}
-            <h5 className="text-md text-white">{projectName}</h5>
-          </div>
+              {/* Project Name  */}
+              <h5 className="text-md text-white font-bold">{projectName}</h5>
+            </summary>
+            {/* Collapsible Content */}
+            <div className="w-full bg-black overflow-y-auto mb-2 ml-0 mr-5 px-1 py-2 pl-4">
+              <p className="text-md text-[#00c9d4]">
+                Laborum voluptate enim proident nulla nisi dolor laborum commodo
+                officia pariatur velit culpa quis. Consectetur excepteur mollit
+                labore eu fugiat ipsum ullamco duis culpa commodo. Ullamco eu
+                Lorem eiusmod do labore nisi sit voluptate nisi labore mollit
+                minim. Aliquip incididunt eiusmod Lorem elit deserunt. Sunt non
+                consequat consequat irure ullamco eiusmod sit qui minim sint
+                elit. Dolor nisi commodo commodo consectetur culpa aliquip velit
+                laboris. Irure nostrud id eu elit non.
+              </p>
+            </div>
+          </details>
         );
       })}
     </div>
